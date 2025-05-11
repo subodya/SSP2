39488274bd4c7a1a7ee1b308f2384371cc4201a3
kaviska <126580314+kaviska@users.noreply.github.com>
2025-05-11 18:12:13 +0530

    user seeder updated
    dome

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,10 +24,10 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'first_name' => fake()->name(),
-            'last_name' => fake()->name(),
+            'name' => fake()->name(),
+            
             'email' =>"kaviska525@gmail.com",
-            'phone' => fake()->unique()->phoneNumber(),
+         
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
               
