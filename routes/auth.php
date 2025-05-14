3299c8a5447092df944828fcca282876765fd066
kaviska <126580314+kaviska@users.noreply.github.com>
2025-05-14 15:59:38 +0530

    updated
    done

--- routes/auth.php
+++ routes/auth.php
@@ -35,5 +35,5 @@ Route::post('/email/verification-notification', [EmailVerificationNotificationCo
     ->name('verification.send');
 
 Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
-    ->middleware('auth')
+   
     ->name('logout');
