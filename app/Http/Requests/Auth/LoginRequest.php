49764f11206dd1a1dc075ee1f60f7871a6606823
kaviska <126580314+kaviska@users.noreply.github.com>
2025-05-14 16:11:33 +0530

    done

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -31,6 +31,7 @@ class LoginRequest extends FormRequest
             'password' => ['required', 'string'],
         ];
     }
+    //smaple comment
 
     /**
      * Attempt to authenticate the request's credentials.
